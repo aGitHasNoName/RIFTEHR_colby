b55f8dd9b86b56dce8aeeedf6ced04717a82626b
Colby Witherup Wood <colby.witherup@northwestern.edu>
2021-12-02 22:02:31 -0600

    initial commit

---
+++ Step2_InferenceAndConflicts/infer.py
@@ -0,0 +1,251 @@
+"""
+"""
+
+from networkx import from_pandas_edgelist
+from networkx import connected_components
+from networkx import number_of_selfloops
+import pandas as pd
+import numpy as np
+from itertools import permutations
+from collections import defaultdict
+import json
+import time
+
+
+#Load all lookup dictionaries
+with open("Step2_InferenceAndConflicts/infer_dict.json", "r") as f:
+    infer_dict = json.load(f)
+with open("Step2_InferenceAndConflicts/opp_lookup.json", "r") as f:
+    opp_lookup = json.load(f)
+with open("Step2_InferenceAndConflicts/age_lookup.json", "r") as f:
+    age_lookup = json.load(f)
+with open("Step2_InferenceAndConflicts/flip_lookup.json", "r") as f:
+    flip_lookup = json.load(f)
+with open("Step2_InferenceAndConflicts/conflict_type_dict.json", "r") as f:
+    conflict_type_dict = json.load(f)
+flip_lookup_older = ["parent", "grandparent"]
+flip_lookup_younger = ["child", "grandchild"]
+
+#Define conflict check booleans - all booleans return the conflicts
+def age_spouse_boolean(p, r, rel):
+    try:
+        return rel == "spouse" and (demo_dict[p][0] < 17 or demo_dict[r][0] < 17)
+    except KeyError:
+        return False
+def age_parent_boolean(p, r, rel):
+    try:
+        return abs(demo_dict[p][0] - demo_dict[r][0]) < age_lookup[rel]
+    except KeyError:
+        return False
+def provided_conflict_boolean(p, r, rel):
+    try:
+        return p in matches[r].keys() and rel != opp_lookup[matches[r][p][0][0]]
+    except KeyError:
+        return False
+def flipped_conflict_boolean(p, r, rel):
+    try:
+        return (rel in flip_lookup_younger and demo_dict[p][0] < demo_dict[r][0]) or (rel in flip_lookup_older and demo_dict[p][0] > demo_dict[r][0])
+    except KeyError:
+        return False
+def duplicate_provided_conflict_boolean(d):
+    return [len([i[1] for i in v]) for v in d.values()] != [len(set([i[1] for i in v])) for v in d.values()]
+
+#Define conflict-handling functions
+def handle_duplicate_provided(d, fam, conflict_status):
+    for p, v in d.items():
+            i_dict = {}
+            for i in v:
+                if i[1] not in i_dict.keys():
+                    i_dict[i[1]] = i[0]
+                elif i[0] != i_dict[i[1]]:
+                    conflict_status = True
+                    conflict_list.append((str(fam), p, i[1], i[0], str(0), conflict_type_dict["dup_provided"]))
+                    matches[p].pop(i[1])
+                    if len(matches[p]) == 0:
+                        matches.pop(p)
+                    conflict_list.append((str(fam), p, i[1], i_dict[i[1]], str(0), conflict_type_dict["dup_provided"]))
+    return conflict_status
+
+def remove_provided_conflict_from_matches(fam, p, r, rel, loop, conflict_type):
+    conflict_list.append((str(fam), p, r, rel, str(loop), conflict_type_dict[conflict_type]))
+    matches[p].pop(r)
+    if len(matches[p]) == 0:
+        matches.pop(p)
+    return True
+
+def infer_check(family):
+    conflict_families = []
+    no_conflict_families = []
+    family_conflict = False
+    global conflict_list
+    conflict_list = []
+    famID = family[0]
+    
+    #create family dictionary
+    family_dict = {p: v for p,v in matches_dict.items() if p in family[1]}
+    global matches
+    matches = {p: {x[1]: [(x[0], 0)] for x in v} for p,v in family_dict.items()}
+    
+    #CHECK for multiple relationships provided for same relation
+    if duplicate_provided_conflict_boolean(family_dict):
+        family_conflict = handle_duplicate_provided(family_dict, famID, family_conflict)
+    
+    #CHECK for age conflicts in provided relationships
+    for p, r in list(permutations(family[1], 2)):
+        try:
+            #is there a provided relationship?
+            provided_rel = matches[p][r][0][0]
+            #remove provided spouse relationship if either spouse is under 17:
+            if age_spouse_boolean(p, r, provided_rel):
+                family_conflict = remove_provided_conflict_from_matches(famID, p, r, provided_rel, 0, "age_spouse")
+            #remove parent/child with <10 year difference, gp/gc <20, etc.
+            elif age_parent_boolean(p, r, provided_rel):
+                family_conflict = remove_provided_conflict_from_matches(famID, p, r, provided_rel, 0, "age_parent")
+            #flip provided age conflicts if parent is younger than child, gp younger than gc, etc.
+            elif flipped_conflict_boolean(p, r, provided_rel):
+                matches[p][r][0] = (flip_lookup[provided_rel], "provided_flipped")
+                #decided to only flag flipped in final output, not add to conflict list
+                #conflict_list.append((str(famID), i[0], i[1], provided_rel, str(0), conflict_type_dict["flip_parent"]))
+        except KeyError:
+            pass  
+    
+    #CHECK for provided conflicts - two people identify each other but relationships aren't opposite
+    #this is separate because if there is an age conflict, we still want the second relationship    
+    for p, r in list(permutations(family[1], 2)):
+        try:
+            provided_rel = matches[p][r][0][0]
+            if provided_conflict_boolean(p, r, provided_rel):
+                family_conflict = remove_provided_conflict_from_matches(famID, p, r, provided_rel, 0, "provided")
+                family_conflict = remove_provided_conflict_from_matches(famID, r, p, matches[r][p][0][0], 0, "provided")
+        except KeyError:
+            pass
+    
+    #Create opposite matches:
+    for i in list(permutations(family[1], 2)):
+        try:
+            opp_rel = opp_lookup[matches[i[0]][i[1]][0][0]]
+            if i[1] not in matches.keys():
+                matches[i[1]] = {i[0]: [(opp_rel, .5)]}
+            elif i[0] not in matches[i[1]].keys():
+                matches[i[1]][i[0]] = [(opp_rel, .5)]
+            elif matches[i[1]][i[0]][0][0] == opp_rel:
+                pass
+        except KeyError:
+            pass
+    
+    #If family has more than two relationships remaining, infer new relationships
+    if len(matches.keys()) > 2:
+        combos = list(permutations(family[1], 3))
+        b = 0
+        check = True
+        while check:
+            a = 0
+            b += 1
+            for com in combos:
+                try:
+                    
+                    #create new relationship
+                    new_rel = infer_dict[matches[com[0]][com[1]][0][0]][matches[com[1]][com[2]][0][0]]
+                    
+                    #CHECK if the new relationship is spouse, are both parties over 16?
+                    if age_spouse_boolean(com[0], com[2], new_rel):
+                        conflict_list.append((str(famID), com[0], com[2], new_rel, str(b), conflict_type_dict["age_spouse"]))
+                        family_conflict = True
+                    
+                    #CHECK if the new relationship is x-parent, is there at least a 10-year gap per generation?
+                    elif age_parent_boolean(com[0], com[2], new_rel):
+                        conflict_list.append((str(famID), com[0], com[2], new_rel, str(b), "age_difference_too_small_for_generation"))
+                        family_conflict = True
+                    
+                    #ready to add the relationship
+                    else:
+                        
+                        #this is a brand new relationship
+                        if com[2] not in matches[com[0]].keys():
+                            matches[com[0]][com[2]] = [(new_rel, b)]
+                        
+                        #this exact relationship already exists
+                        elif new_rel == matches[com[0]][com[2]][0][0]:
+                            pass
+                        
+                        #there is a less specific relationship than already exists
+                        elif new_rel in matches[com[0]][com[2]][0][0].split("_"):
+                            matches[com[0]][com[2]][0] = (new_rel, b)
+                        
+                        #there is a more specific relationship that already exists
+                        elif matches[com[0]][com[2]][0][0] in new_rel.split("_"):
+                            pass
+                        
+                        #CHECK there is a conflict with a relationship that already exists
+                        else:    
+                            matches[com[0]][com[2]].append((new_rel, b))
+                            conflict_list.append((str(famID), com[0], com[2], new_rel, str(b), "conflict_in_inferred_relationship"))
+                            family_conflict = True
+                            
+                    combos.remove(com)
+                    a +=1
+                
+                #there isn't a third party relationship on this pass, but there might be later
+                except KeyError:
+                    pass
+            
+            #there were no new inferred relationships on this pass, so all possible inferences have been made
+            if a == 0:
+                check = False
+    
+    #add data to final lists
+    if family_conflict == False:
+        for p,v in matches.items():
+            for r, tup_list in v.items():
+                for tup in tup_list:
+                    no_conflict_families.append((str(famID), p, tup[0], r, str(tup[1]), str(demo_dict[p][0]), str(demo_dict[r][0]), demo_dict[p][1], demo_dict[r][1]))
+    else:     
+        for i in conflict_list:
+            conflict_families.append((i[0], i[1], i[3], i[2], i[4], str(demo_dict[i[1]][0]), str(demo_dict[i[2]][0]), demo_dict[i[1]][1], demo_dict[i[2]][1], i[5]))
+        for p,v in matches.items():
+            for r, tup_list in v.items():
+                for tup in tup_list:
+                    conflict_families.append((str(famID), p, tup[0], r, str(tup[1]), str(demo_dict[p][0]), str(demo_dict[r][0]), demo_dict[p][1], demo_dict[r][1], "no_primary_conflict"))
+    return conflict_families, no_conflict_families
+
+def step_two(df, patient_info_file, ec_info_file):
+    #Load all data and convert to dictionaries
+    demo_pt_df = pd.read_csv(patient_info_file, dtype=str).replace(np.nan, '')
+    demo_ec_df = pd.read_csv(ec_info_file, dtype=str).replace(np.nan, '')
+    demo_dict_pt = {mrn: (int(age), sex) for mrn, age, sex in zip(demo_pt_df["MRN"], demo_pt_df["Age"], demo_pt_df["Sex"])}
+    demo_dict_ec = {mrn: (int(age), sex) for mrn, age, sex in zip(demo_ec_df["MRN_1"], demo_ec_df["Age"], demo_ec_df["Sex"])}
+    global demo_dict
+    demo_dict = {**demo_dict_ec, **demo_dict_pt}
+    global matches_dict
+    matches_dict = defaultdict(list)
+    for index, row in df.iterrows():
+        if row['pt_mrn'] != row['matched_mrn']:
+            matches_dict[str(row['pt_mrn'])].append((row['ec_relation'], str(row['matched_mrn'])))
+    
+    #Create families
+    F = from_pandas_edgelist(df, "pt_mrn", "matched_mrn")
+    c = [s for s in connected_components(F) if len(s) > 1]
+    
+    #Check for self-matches
+    if number_of_selfloops(F) > 0:
+        print(f"{number_of_selfloops(F)} self matches.")
+    
+    #Sort families largest to smallest
+    c.sort(key=len, reverse=True)
+    
+    #assign family IDs
+    c_dict = {i:list(fam) for i, fam in zip(range(1, len(c)+1), c)}
+
+    #Run conflict checks and inferences and format output
+    time_step1 = time.time()
+    results = [infer_check(x) for x in c_dict.items()]
+    time_step2 = time.time()
+    print("Time Taken for inference: ", time_step2 - time_step1)
+    nc_families = [i for f in results for i in f[1]]
+    final_nc_families = [("famID", "pt_mrn", "ec_relation", "matched_mrn", "inference_pass", "pt_age", "mactched_age", "pt_sex", "matched_sex")] + nc_families
+    c_families = [i for f in results for i in f[0]]
+    final_c_families = [("famID", "pt_mrn", "ec_relation", "matched_mrn", "inference_pass", "pt_age", "mactched_age", "pt_sex", "matched_sex", "conflict_type")] + c_families
+
+    return final_c_families, final_nc_families
+    
+#step_two(input_df, patient_info, ec_info)
---
+++ main_script.py
@@ -0,0 +1,150 @@
+# Central script to run RIFTEHR program.
+# 
+# USAGE:
+#   -----
+#   From Command Line:
+#   python main_script.py <Absolute Path to Inputs Folder> <Patient_Demographics_Table_Name.csv> <Emergency_Contact_Table_Name.csv> <relation_map.csv> (--skip_preprocessing)
+# 
+# Optional Parameters:
+#   --skip_preprocessing - If you have already preprocessed and would like to save time, you may set this field.
+# You can then give paths to the preprocessed files instead when you call the script.
+# 
+# For example:
+#   python "C:\Users\research user\input folder\" All_Patients.csv Emergency_Contacts.csv relation_map.csv
+# 
+# You may also define the default paths and variables below, and simply run the program as:
+#     python main_script.py
+# 
+# AUTHORS:
+# -------
+# Original Version: Fernanda Polubriaginof and Nicholas Tatonetti
+# 2nd Version: Farhad Ghamsari
+# This Version: Colby Wood and Dan Turner
+
+
+import os
+import sys
+import time
+import pandas as pd
+import numpy as np
+from pathlib import Path  # Aiming for cross Windows/Mac/Linux compatibility: we will use Python 3's pathlib for all paths
+
+
+# Default Paths: These paths will be used if running from command line without supplied arguments, or if running main_script.py from within an IDE
+# main_inputs_path contains your input_files and also serves as the output folder
+# This folder can be separate from the folder which contains your code
+main_inputs_path = Path("/Volumes/fsmresfiles/PrevMed/Projects/Family_Linkage")
+
+# Paths to the input files themselves
+patients_file_path = Path(main_inputs_path / "100K SAMPLE/100k_patientsample.csv")
+emergency_contacts_file_path = Path(main_inputs_path / "100K SAMPLE/100k_emergencycontactsample.csv")
+relation_map_file_path = Path(main_inputs_path / "relation_map.csv")
+
+# Path to preprocessed files: If you have set the skip_preprocessing flag below to True, then ensure these paths are correct
+preprocessed_pt_fp = Path(main_inputs_path / "preprocessed_To_useAllPatients_TableFinal_withConflicts.csv")
+preprocessed_ec_fp = Path(main_inputs_path / "preprocessed_To_useEmergencyContact_TableFinal_withConflicts.csv")
+
+# Flags
+skip_preprocessing = False # Set this flag if you have already preprocessed once and would like to save some time
+encrypt_first = False # Encryption is not currently implemented
+skip_hashing = False # Not currently implemented
+debugging = False # Not currently implemented
+skip_writing = False # Set this flag True if you don't want to write any data (for testing purposes)
+
+pt_df = pd.DataFrame()        # container for patient data
+ec_df = pd.DataFrame()        # container for emergency contact data
+id_conflicts = pd.DataFrame() # container for conflicting relationships
+
+def the_work():
+    global preprocessed_pt_fp
+    global preprocessed_ec_fp
+
+    # Begin Step 0 - PreProcessing
+    time_step0 = time.time()
+
+    import Step0_PreProcessing.preprocess as PreProcessor
+    # Pre-process the dataframes - See script for steps.
+    if not skip_writing and not skip_preprocessing: # paths not created if skip_writing is True
+        preprocessed_pt_fp, preprocessed_ec_fp = PreProcessor.preprocess(patients_file_path, emergency_contacts_file_path, relation_map_file_path)
+
+    # Todo: If ever you implement encryption, this is where it will go
+    time_step1 = time.time()
+    print("Time Taken for Step 0: ", time_step1 - time_step0)
+    
+    # Begin Step 1 - Identify matches across the patient and emergency_contact dataframes; Output goes to matched_table.csv
+    from Step1_MatchECtoDemog.match_in_batches import BatchMatcher
+    match_df = BatchMatcher(main_inputs_path, preprocessed_pt_fp, preprocessed_ec_fp, id_conflicts = None, skip_hashing = skip_hashing).run() # Pass #1
+
+    time_step2 = time.time()
+    print("Time Taken for Step 1: ", time_step2 - time_step1)
+
+    # Begin Step 2 - Relationship Inference - Infer relationships from match_df
+       
+    from Step2_InferenceAndConflicts.infer import step_two
+    
+    conflict, no_conflict = step_two(match_df, preprocessed_pt_fp, preprocessed_ec_fp)
+    
+    time_step3 = time.time()
+    print("Time Taken for Step 2: ", time_step3 - time_step2)
+
+    # Write final files
+    
+    time_stepWrite1 = time.time()
+    with open(Path(main_inputs_path / "no_conflict_families.csv"), "w") as f:
+        for line in no_conflict:
+            f.write(",".join(line) + "\n")
+    with open(Path(main_inputs_path / "conflict_families.csv"), "w") as f:
+        for line in conflict:
+            f.write(",".join(line) + "\n")
+    time_stepWrite2 = time.time()
+    print("Time Taken to write: ", time_stepWrite2 - time_stepWrite1)
+    
+    
+    
+if __name__ == '__main__':
+    print("Arguments provided: ", sys.argv)
+
+    # First checks if enough arguments were provided. If not, program uses defaults at top of script.
+    if len(sys.argv) < 5:  # sys.argv[0] is the main_script.py's path
+        print("\tNot enough arguments provided - expected at least 4:\n\t\t1) Main input path, \n\t\t2) Patient Demographics file path, \n\t\t3) Emergency Contacts file path\n\t\t4) Relationship Map file path\n\n\tWill use default paths and parameters as defined at top of main_script.py")
+
+        # Validating default parameters as found at top of this script
+        assert os.path.exists(main_inputs_path), "Main Input Folder Path is invalid. Modify value at top of main_script.py"
+        assert os.path.exists(patients_file_path), "Default Patient Demographics File Path is invalid. Modify value at top of main_script.py"
+        assert os.path.exists(emergency_contacts_file_path), "Default Emergency Contacts File Path is invalid. Modify value at top of main_script.py"
+        assert os.path.exists(relation_map_file_path), "Default Relationship Map File Path is invalid. Modify value at top of main_script.py"
+
+        if skip_preprocessing:
+            print("\t`skip_preprocessing' flag is set to True. Will skip preprocessing.\n")
+            assert os.path.exists(preprocessed_pt_fp), "Default PreProcessed Patient Demographics Path is invalid. Modify value at top of main_script.py"
+            assert os.path.exists(preprocessed_ec_fp), "Default PreProcessed Emergency Contacts File path is invalid.  Modify value at top of main_script.py"
+
+    # If enough arguments were provided, will validate parameters then begin running.
+    else:
+        tmp_main_path = Path(sys.argv[1])
+        assert os.path.exists(tmp_main_path), "Main Inputs Folder was not found. Exiting."
+
+        tmp_pt_path = Path(tmp_main_path / sys.argv[2])
+        assert os.path.exists(tmp_pt_path), "Patient Demographics File was not found. Exiting."
+
+        tmp_ec_path = Path(tmp_main_path / sys.argv[3])
+        assert os.path.exists(tmp_ec_path), "Emergency Contacts File was not found. Exiting."
+
+        tmp_reln_path = Path(tmp_main_path / sys.argv[4])
+        assert os.path.exists(relation_map_file_path), "Relationship Mapping File was not found. Exiting."
+
+        main_inputs_path = tmp_main_path
+        patients_file_path = tmp_pt_path
+        emergency_contacts_file_path = tmp_ec_path
+        relation_map_file_path = tmp_reln_path
+
+        if "--skip_preprocessing" in sys.argv:
+            skip_preprocessing = True
+            preprocessed_pt_fp = sys.argv[2]
+            preprocessed_ec_fp = sys.argv[3]
+            print("Skipping Pre-Processing, using provided paths as paths to preprocessed files.")
+        else:
+            skip_preprocessing = False
+
+    the_work()
+
